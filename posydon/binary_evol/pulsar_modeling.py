0abff2a54d8432cc8aa9eca013b491251562dab1
Jorie286 <mcdermott.286@osu.edu>
2025-11-14 13:33:27 -0500

    Force astropy.constants to return just the value
    Updated constants to use .value so that no units are given in calculations. Not including this his has cause numerous errors across calculations involving mass, and radius.

--- posydon/binary_evol/pulsar_modeling.py
+++ posydon/binary_evol/pulsar_modeling.py
@@ -66,31 +66,31 @@ class Pulsar:
             alpha = 30.0: rough value for the angle between the magnetic and rotational axis in degrees
         '''
 
-        NS_RADIUS = CO_radius(initial_mass, "NS")*astro_const.R_sun  # POSYDON constant for NS radius [m] 
+        NS_RADIUS = CO_radius(initial_mass, "NS")*astro_const.R_sun.value  ## POSYDON constant for NS radius [m] 
         
-        self.mass = initial_mass*astro_const.M_sun            # mass of the NS [kg]
-        self.radius = NS_RADIUS                               # radius of the NS [m]
-        self.moment_inertia = self.calc_moment_of_inertia()   # moment of inertia of the NS  [kg m^2]
-        # Note: Because the NS radius is constant, moment of inertia should also be constant throughout NS evolution
+        self.mass = initial_mass*astro_const.M_sun.value            ## mass of the NS [kg]
+        self.radius = NS_RADIUS                               ## radius of the NS [m]
+        self.moment_inertia = self.calc_moment_of_inertia()   ## moment of inertia of the NS  [kg m^2]
+        ## Note: Because the NS radius is constant, moment of inertia should also be constant throughout NS evolution
         
-        self.alpha = alpha*const.a2rad                        # angle between magnetic and rotational axis [rad]
-        self.Mdot_edd = self.calc_NS_edd_lim(np.nan)          # Eddington accretion rate for the NS [Msun/yr]
-        self.luminosity = self.calc_NS_luminosity()           # radio luminosity of the pulsar [J/s]
+        self.alpha = alpha*const.a2rad                        ## angle between magnetic and rotational axis [rad]
+        self.Mdot_edd = self.calc_NS_edd_lim(np.nan)          ## Eddington accretion rate for the NS [Msun/yr]
+        self.luminosity = self.calc_NS_luminosity()           ## radio luminosity of the pulsar [J/s]
 
-        # if magnetic field and spin not specified when initializing the pulsar,
-        # draw random values
+        ## if magnetic field and spin not specified when initializing the pulsar,
+        ## draw random values
         if np.isnan(B_field):
-            self.Bfield = self.draw_NS_Bfield()      # NS magnetic field [T]
+            self.Bfield = self.draw_NS_Bfield()      ## NS magnetic field [T]
         else:
             self.Bfield = B_field
 
         if np.isnan(spin):
-            self.spin = self.draw_NS_spin()          # NS spin angular frequency [1/s]
+            self.spin = self.draw_NS_spin()          ## NS spin angular frequency [1/s]
         else:
             self.spin = spin
             
         if np.isna(P_dot):
-            self.P_dot = self.calc_NS_spindown_rate()    # initial calculation for NS spindown rate [unitless]
+            self.P_dot = self.calc_NS_spindown_rate()    ## initial calculation for NS spindown rate [unitless]
         else:
             self.P_dot = P_dot
 
@@ -118,10 +118,10 @@ class Pulsar:
         Calculate the moment of intertia of the NS [kg*m^2]
         Eq. from Kiel et al. 2008 is in solar units
         '''
-        M = self.mass/astro_const.M_sun       # mass of the NS [Msun] 
-        R = self.radius/astro_const.R_sun     # radius of the NS [Rsun]
+        M = self.mass/astro_const.M_sun.value       # mass of the NS [Msun] 
+        R = self.radius/astro_const.R_sun.value     # radius of the NS [Rsun]
 
-        return 2/7 * (1 - 2.42e-6*M/R - 2.9e-12*M**2/R**2)**-1 * M*R**2 * (astro_const.M_sun*astro_const.R_sun**2)
+        return 2/7 * (1 - 2.42e-6*M/R - 2.9e-12*M**2/R**2)**-1 * M*R**2 * (astro_const.M_sun.value*astro_const.R_sun.value**2)
     
     def calc_NS_edd_lim(self, surface_h1):
         """
@@ -145,8 +145,8 @@ class Pulsar:
 
         #eta = const.standard_cgrav*self.mass / (self.radius * const.clight**2)
         #Mdot_edd = (4*np.pi*const.standard_cgrav*self.mass) / (0.2*(1 + surface_h1)*eta*const.clight)  
-        Mdot_edd = (4 * np.pi * astro_const.m_p * self.radius * astro_const.c) / astro_const.sigma_T 
-        return Mdot_edd / (astro_const.M_sun/const.secyer)      # convert [kg/s] to [Msun/yr]
+        Mdot_edd = (4 * np.pi * astro_const.m_p.value * self.radius * astro_const.c.value) / astro_const.sigma_T.value
+        return Mdot_edd / (astro_const.M_sun.value/const.secyer)      # convert [kg/s] to [Msun/yr]
 
     def calc_NS_luminosity(self):
         """
@@ -154,7 +154,7 @@ class Pulsar:
         units of distribution from Szary et al. 2014 are mJy*kpc^2
         Returns the radio luminosity in unis of [J/s]
         """
-        kpc2cm = astro_const.kpc    # kpc to m
+        kpc2cm = astro_const.kpc.value    # kpc to m
         mJy2si = 1e-29              # milliJansky in SI units [J/s/m^2/Hz]
         Hz = 1400*1e6               # Hz conversion for radio luminosity (1400 MHz)
         mJykpc2si = mJy2si*kpc2m**2*Hz
@@ -172,9 +172,10 @@ class Pulsar:
         alpha = self.alpha # angle between magnetic and rotational axis [rad]
 
         # get the change in spin [1/s^2], from Chattopadhyay et al. 2020
-        omega_dot = - (8 * np.pi * (self.Bfield**2) * (self.radius**6) * (self.spin**3) * (np.sin(alpha))**2) / (3 * astro_const.mu0 * (astro_const.c**3) * I)
+        omega_dot = - (8 * np.pi * (self.Bfield**2) * (self.radius**6) * (self.spin**3) * (np.sin(alpha))**2) / (3 * astro_const.mu0.value * (astro_const.c.value**3) * I)
         
         #P_dot =  9.87e-48*const.secyer * self.Bfield**2/P    # NS spindown rate
+        P = (2*np.pi)/self.spin      # NS spin period
         P_dot = - (omega_dot * P) / self.spin # NS spindown rate from Chattopadhyay et al. 2020
         
         return P_dot
@@ -184,7 +185,7 @@ class Pulsar:
         Calculate the spindown power [J/s] of the NS.
         From Szary et al. 2019
         """
-        P = 2*np.pi/self.spin      # NS spin period
+        P = (2*np.pi)/self.spin      # NS spin period
         P_dot = self.P_dot         # NS spindown rate
 
         I = self.moment_inertia # get the moment of interia [kg m^2]
@@ -207,14 +208,14 @@ class Pulsar:
         float
             r_m: alfven radius of the NS [m]
         '''
-        Mdot_acc *= (astro_const.M_sun/const.secyer)  # convert [Msun/yr] to [kg/s]
+        Mdot_acc *= (astro_const.M_sun.value/const.secyer)  # convert [Msun/yr] to [kg/s]
 
         #eta = 0.4    # constant depending on disk-magnetosphere interaction
         #mu = self.Bfield * self.radius**3     # NS mangetic moment
         #r_m = eta * (mu**4/(2*const.standard_cgrav*self.mass*Mdot_acc**2))**(1/7)
 
         # get the alfven radius [m], from Chattopadhyay et. al. 2020
-        r_m = ((2 * np.pi**2 * self.radius**12 * self.Bfield**4)/(astro_const.G * astro_const.mu0**2 * Mdot_acc**2 * self.mass))**(1/7)
+        r_m = ((2 * np.pi**2 * self.radius**12 * self.Bfield**4)/(astro_const.G.value * astro_const.mu0.value**2 * Mdot_acc**2 * self.mass))**(1/7)
         
         return r_m
 
@@ -234,7 +235,7 @@ class Pulsar:
         '''
         r_m = self.calc_magnetosphere_radius(Mdot_acc) # Alfven radius [m]
         
-        omega_eq = 1/(2*np.pi) * (astro_const.G*self.mass/r_m**3)**(1/2) # equillibrium spin [1/s]
+        omega_eq = 1/(2*np.pi) * (astro_const.G.value*self.mass/r_m**3)**(1/2) # equillibrium spin [1/s]
         
         return omega_eq
 
@@ -254,7 +255,7 @@ class Pulsar:
             neutron star spin frequency at the Keplarian limit [1/s]
         '''
         R_ns = self.radius/(1e3*10) # convert the neutron star radius from m to units of 10 km
-        f_k = C * np.sqrt(self.mass/astro_const.M_sun) * (R_ns**(-3/2)) # unints of kHz
+        f_k = C * np.sqrt(self.mass/astro_const.M_sun.value) * (R_ns**(-3/2)) # unints of kHz
         return f_k * 1e3 # return the spin frequency in units of Hz for more convenient use
 
     def Vdiff_fnct(self, R_mag):
@@ -277,7 +278,7 @@ class Pulsar:
 
         Omega_co = (2*np.pi)*self.spin # get the angular velocity of rotation (same as the co-rotation angular velocity) [Hz]  
         
-        Omega_k = np.sqrt(astro_const.G*self.mass/R_mag**3) # from COMPAS function [Hz]
+        Omega_k = np.sqrt(astro_const.G.value*self.mass/R_mag**3) # from COMPAS function [Hz]
         
         return Omega_k - Omega_co # units of Hz
    
@@ -292,7 +293,7 @@ class Pulsar:
             tau_d: the Bfield decay timescale [Gyr]
         '''
         alpha = self.alpha              # angle between the axis of rotation and magnetic axis [rad]
-        mu_0 = astro_const.mu0          # permeability of free space [N/A^2]
+        mu_0 = astro_const.mu0.value          # permeability of free space [N/A^2]
         B_min = 1e8*1e-4                # minimum Bfield strength at which Bfield decay ceases [T]
         tau_d *= (1e9*const.secyer)     # B-field decay timescale [s]
 
@@ -307,7 +308,7 @@ class Pulsar:
         self.Bfield = B_f
 
         # evolve the NS spin
-        A = 8 * np.pi * R**6 * np.sin(alpha)**2 / (3 * mu_0 * astro_const.c**3 * I)    # [s^5 A^2/kg^2]
+        A = 8 * np.pi * R**6 * np.sin(alpha)**2 / (3 * mu_0 * astro_const.c.value**3 * I)    # [s^5 A^2/kg^2]
         omega_f = np.sqrt(1/( A*(B_min**2*delta_t - tau_d*B_min*(B_f - B_i) - tau_d/2*(B_f**2 - B_i**2))  + 1/self.spin**2))    # [1/s]
         self.spin = omega_f
 
@@ -332,12 +333,12 @@ class Pulsar:
             delta_Md: the magmetic field mass decay scale [Msun]
         '''
 
-        G = astro_const.G             # gravitational constant [c^3 kg^-1 s^-2]
-        mu_0 = astro_const.mu0        # permeability of free space [N/A^2]
+        G = astro_const.G.value             # gravitational constant [c^3 kg^-1 s^-2]
+        mu_0 = astro_const.mu0.value        # permeability of free space [N/A^2]
         tau_d *= (1e9*const.secyer)   # B-field decay timescale [s]
-        delta_Md *= astro_const.M_sun # magnetic field mass decay scale [kg]
+        delta_Md *= astro_const.M_sun.value # magnetic field mass decay scale [kg]
 
-        delta_M *= astro_const.M_sun  # mass accreted by pulsar during RLO [kg]
+        delta_M *= astro_const.M_sun.value  # mass accreted by pulsar during RLO [kg]
         delta_t *= const.secyer       # duration of RLO accretion [s]
 
         M_i = self.mass               # mass of the NS before accretion [kg]
@@ -393,13 +394,13 @@ class Pulsar:
         Mdot_acc: float, mass accretion rate onto the pulsar  [Msun/yr]
         CE: bool, True/False if the pulsar is accreting in a common envelope state
         '''
-        G = astro_const.G             # gravitational constant [m^3 kg^-1 s^-2]
-        delta_Md *= astro_const.M_sun # magnetic field mass decay scale [kg]
+        G = astro_const.G.value             # gravitational constant [m^3 kg^-1 s^-2]
+        delta_Md *= astro_const.M_sun.value # magnetic field mass decay scale [kg]
         B_min = 1e8*1e-4              # minimum Bfield strength at which Bfield decay ceases [T]
-        mu_0 = astro_const.mu0        # permeability of free space [N/A^2]
+        mu_0 = astro_const.mu0.value        # permeability of free space [N/A^2]
         efficiency = 1                # set the pulsar to the maximum accretion efficiency to start
 
-        delta_M *= astro_const.M_sun  # convert Msun to kg
+        delta_M *= astro_const.M_sun.value  # convert Msun to kg
 
         M_i = self.mass               # mass of the NS before accretion [kg]  
         R = self.radius               # radius of the NS [m]
@@ -433,10 +434,10 @@ class Pulsar:
 
         # calculate the rate of change of angular momentum using Vdiff
         V_diff = self.Vdiff_fnct(R_mag)    # [1/s]
-        J_dot = efficiency * V_diff * (R_mag**2) * ((Mdot_acc*astro_const.M_sun)/const.secyer)    # [kg m^2/s^2]
+        J_dot = efficiency * V_diff * (R_mag**2) * ((Mdot_acc*astro_const.M_sun.value)/const.secyer)    # [kg m^2/s^2]
 
         # calculate the amount of time in each delta_M step and use this to calculate delta_J (IS THIS THE RIGHT WAY TO FIX THE ISSUE OF J_DOT_ACC UNIT MISMATCH???)
-        delta_t = delta_M/((Mdot_acc*astro_const.M_sun)/const.secyer)    # [s]
+        delta_t = delta_M/((Mdot_acc*astro_const.M_sun.value)/const.secyer)    # [s]
         delta_J = J_dot*delta_t    # [kg m^2/s]
         
         J_f = J_i + delta_J # [kg m^2/s]
